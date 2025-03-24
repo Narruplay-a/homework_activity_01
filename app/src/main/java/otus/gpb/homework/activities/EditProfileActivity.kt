2660186a36f9475197602a12946813f67fe00512
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2025-03-24 15:51:21 +0300

    homework activity 3

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,20 +1,85 @@
 package otus.gpb.homework.activities
 
+import android.Manifest
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlin.time.Duration
 
 class EditProfileActivity : AppCompatActivity() {
-
     private lateinit var imageView: ImageView
+    private lateinit var editButton: Button
+    private lateinit var nameTw: TextView
+    private lateinit var lastnameTw: TextView
+    private lateinit var ageTw: TextView
+    private var imageUri: Uri = Uri.EMPTY
+
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val data = result.data
+        val resultCode = result.resultCode
+        if (resultCode == RESULT_OK && data != null) {
+            nameTw.text = data.getStringExtra(NAME_KEY)
+            lastnameTw.text = data.getStringExtra(LASTNAME_KEY)
+            ageTw.text = data.getStringExtra(AGE_KEY)
+
+        } else {
+            Toast.makeText(this, "Вы не ввели всех данных", Toast.LENGTH_SHORT)
+        }
+    }
+
+    private val permissionCamera = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        when {
+            granted -> {
+                imageView.setImageResource(R.drawable.cat)
+            }
+
+            !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle(resources.getString(R.string.camera_access))
+                    .setMessage(resources.getString(R.string.photo_reason))
+                    .setPositiveButton(resources.getString(R.string.open_settings)) { dialog, which ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                            data = Uri.fromParts("package", packageName, null)
+                        }
+                        startActivity(intent)
+                    }
+                    .show()
+            }
+            else -> {  }
+        }
+    }
+
+    private val takePictureUri = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            populateImage(uri)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
         imageView = findViewById(R.id.imageview_photo)
+        editButton = findViewById(R.id.button4)
+        nameTw = findViewById(R.id.textview_name)
+        lastnameTw = findViewById(R.id.textview_surname)
+        ageTw = findViewById(R.id.textview_age)
 
         findViewById<Toolbar>(R.id.toolbar).apply {
             inflateMenu(R.menu.menu)
@@ -28,6 +93,14 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
+
+        editButton.setOnClickListener {
+            launcher.launch(Intent(this, FillFormActivity::class.java))
+        }
+
+        imageView.setOnClickListener {
+            showImageSelectDialog()
+        }
     }
 
     /**
@@ -38,7 +111,47 @@ class EditProfileActivity : AppCompatActivity() {
         imageView.setImageBitmap(bitmap)
     }
 
+    private fun showImageSelectDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.title))
+            .setMessage(resources.getString(R.string.select_an_action))
+            .setNeutralButton(resources.getString(R.string.take_a_photo)) { dialog, which ->
+                val rew = shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+
+                if (!rew) {
+                    permissionCamera.launch(Manifest.permission.CAMERA)
+                } else {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle(resources.getString(R.string.camera_access))
+                        .setMessage(resources.getString(R.string.photo_reason))
+                        .setPositiveButton(resources.getString(R.string.give_access)) { _, _ ->
+                            permissionCamera.launch(Manifest.permission.CAMERA)
+                        }
+                        .setNeutralButton(resources.getString(R.string.cancellation)) { _, _ ->
+                        }
+                        .show()
+                }
+            }
+            .setPositiveButton(resources.getString(R.string.choose_a_photo)) { _, _ ->
+                takePictureUri.launch("image/*")
+            }
+            .show()
+    }
+
     private fun openSenderApp() {
-        TODO("В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку")
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "image/*"
+            setPackage("org.telegram.messenger")
+            action = Intent.ACTION_SEND
+            if (!Uri.EMPTY.equals(imageUri)) {
+                putExtra(Intent.EXTRA_STREAM, imageUri)
+            }
+            putExtra(
+                Intent.EXTRA_TEXT,
+                "${nameTw.text}\n${lastnameTw.text}\n${ageTw.text}"
+            )
+        }
+
+        startActivity(shareIntent)
     }
 }
---
+++ app/src/main/java/otus/gpb/homework/activities/FillFormActivity.kt
@@ -0,0 +1,61 @@
+package otus.gpb.homework.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+const val NAME_KEY = "name_key"
+const val LASTNAME_KEY = "lastname_key"
+const val AGE_KEY = "age_key"
+
+class FillFormActivity : AppCompatActivity() {
+    private lateinit var okButton: Button
+    private lateinit var nameTe: TextInputEditText
+    private lateinit var lastnameTe: TextInputEditText
+    private lateinit var ageTe: TextInputEditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fill_form)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        okButton = findViewById(R.id.ok_btn)
+        nameTe = findViewById(R.id.name_ed)
+        lastnameTe = findViewById(R.id.lastname_ed)
+        ageTe = findViewById(R.id.age_ed)
+
+        okButton.setOnClickListener {
+            handleOkButtonClick()
+        }
+    }
+
+    private fun handleOkButtonClick() {
+        val name = nameTe.text.toString()
+        val lastname = lastnameTe.text.toString()
+        val age = ageTe.text.toString()
+
+        if (name != "" || lastname != "" || age != "") {
+            val intent = Intent().apply {
+                putExtra(NAME_KEY, name)
+                putExtra(LASTNAME_KEY, lastname)
+                putExtra(AGE_KEY, age)
+            }
+
+            setResult(RESULT_OK, intent)
+        } else {
+            setResult(RESULT_CANCELED)
+        }
+
+        finish()
+    }
+}
